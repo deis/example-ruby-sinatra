861fbff6902e1c1c6630a9f532a78d902b097055
Joshua Anderson <joshua@deis.com>
2016-08-25 13:57:02 -0700

    feat(*): match example-go output

--- web.rb
+++ web.rb
@@ -2,8 +2,13 @@ require 'sinatra'
 
 set :port, ENV["PORT"] || 5000
 
+get '/healthz' do
+  status 200
+end
+
 get '/' do
   whom = ENV["POWERED_BY"] || "Deis"
+  revision = ENV['WORKFLOW_RELEASE'] || "unknown"
   container = `hostname`.strip || "unknown"
-  "Powered by " + whom + "\nRunning on container ID " + container + "\n"
+  "Powered by #{whom}\nRelease #{revision} on #{container}\n"
 end
